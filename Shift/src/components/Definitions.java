35ec0a0a02f59caa83daccb6ba62ed2ef10f28c2
Lightcyan21 <jonas.baldowe1@web.de>
2015-02-18 16:26:44 +0100

    Logos hinzugefügt und Fenstereinstellungen des ShiftFrames angepasst.

---
+++ Shift/src/components/Definitions.java
@@ -0,0 +1,8 @@
+package components;
+
+import java.awt.Color;
+
+public class Definitions {
+static final String TITLE = "SHIFT - GEBÄUDEMANAGEMENT";
+static final Color BG_COLOR = Color.ORANGE;
+}
--- Shift/src/components/ShiftFrame.java
+++ Shift/src/components/ShiftFrame.java
@@ -1,10 +1,10 @@
 package components;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.CardLayout;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -14,25 +14,39 @@ public class ShiftFrame extends JFrame {
 
 	public ShiftFrame() throws HeadlessException {
 		super();
-		// Setzen der Fenstereisntellungen
+		// Setzen der Fenstereinstellungen
 		setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		//setBackground(Color.ORANGE);
-		setLayout(new BorderLayout());
-		JPanel jpanel = new JPanel();
-		jpanel.setBackground(Color.ORANGE);
-		add(jpanel,BorderLayout.CENTER);
-		//Schlieﬂen der Anwendung bei Klick auf X
+		setTitle(Definitions.TITLE);
+		// Schlieﬂen der Anwendung bei Klick auf X
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setResizable(false);
-		//setFocusable(true);
-		//setUndecorated(true);
-		//sichtbar werden lassen
+
+		try {
+			ImageIcon ii = new ImageIcon("res/Logo2.png");
+			System.out.println(ii.getImage().toString());
+			System.out.println(ii.getImage() == null);
+			setIconImage(ii.getImage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// setResizable(false);
+		// setFocusable(true);
+		// setUndecorated(true);
+		// sichtbar werden lassen
 		setVisible(true);
+
+		// Layout
+		CardLayout cl = new CardLayout();
+		setLayout(cl);
+		JPanel jpanel = new JPanel();
+		jpanel.setBackground(Definitions.BG_COLOR);
+		add(jpanel, "Home");
+
 	}
 
-	public ShiftFrame(String arg0) throws HeadlessException {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
+//	public ShiftFrame(String arg0) throws HeadlessException {
+//		super(arg0);
+//		// TODO Auto-generated constructor stub
+//	}
 
 }
