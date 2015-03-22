916e455c5505daa3180503ecf46e1a34973eb702
Anki <Anki@Anki-PC.fritz.box>
2015-03-22 21:24:34 +0100

    shift Button2

---
+++ Shift/src/components/ShiftButton2.java
@@ -0,0 +1,23 @@
+package components;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class ShiftButton2 extends JButton {
+	
+		private static final long serialVersionUID = -8086832785823587028L;
+
+		public ShiftButton2(String text) {
+			super(text);
+			setFont(new Font("Arial Black", Font.BOLD, 20));
+			setOpaque(false);
+			setBackground(new Color (0,0,0,0));
+			setPreferredSize(new Dimension(233, 73));
+			setBorder (null);
+			}
+}
