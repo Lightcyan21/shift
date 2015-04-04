5a9749c6bc99aaa8ff9caa85010b7c695245a89a
Anki <Anki@Anki-PC.fritz.box>
2015-04-04 13:50:51 +0200

    Search-Button

--- Shift/src/components/ShiftButtonBack.java
+++ Shift/src/components/ShiftButtonBack.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ShiftButtonBack extends JButton {
@@ -15,14 +16,15 @@ public class ShiftButtonBack extends JButton {
 
 	public ShiftButtonBack(){
 		super();
-		setFont(new Font("Arial Unicode MS", Font.BOLD, 15));
-		setText("\u25C4 Zurück");
+//		setFont(new Font("Arial Unicode MS", Font.BOLD, 15));
+//		setText("\u25C4 Zurück");
 		
-		setPreferredSize(new Dimension(125,30));
-//		setHorizontalAlignment(SwingConstants.C );
+		setPreferredSize(new Dimension(125,43));
+					
+		setIcon(new ImageIcon("res/Zurück.png"));
 		
-		setBackground(Color.LIGHT_GRAY);
-		setForeground(Color.DARK_GRAY);
+		
+		
 		
 		
 			
